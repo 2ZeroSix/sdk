c3c13a1ad09f86ccc0ce8d5aafdc6589ecdb938d
Alexander Markov <alexmarkov@google.com>
2024-01-29 23:44:59 +0000

    [vm/compiler] Fix truncation of class id when it is stored in FieldGuardState
    Class id can occupy more than 16 bits, so bit field
    FieldGuardState::GuardedCidBits is extended. Also added assertion that
    it has at least target::UntaggedObject::kClassIdTagSize bits.
    
    TEST=ci, manually tested repro from b/322790241.
    Fixes b/322790241
    
    Change-Id: I39d0592a5a1c8e0c83b7af7d30de6f966b358fda
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/349082
    Reviewed-by: Ryan Macnak <rmacnak@google.com>
    Commit-Queue: Alexander Markov <alexmarkov@google.com>

--- runtime/vm/compiler/backend/slot.cc
+++ runtime/vm/compiler/backend/slot.cc
@@ -318,7 +318,10 @@ const Slot& Slot::GetCanonicalSlot(Thread* thread,
 
 FieldGuardState::FieldGuardState(const Field& field)
     : state_(GuardedCidBits::encode(field.guarded_cid()) |
-             IsNullableBit::encode(field.is_nullable())) {}
+             IsNullableBit::encode(field.is_nullable())) {
+  ASSERT(compiler::target::UntaggedObject::kClassIdTagSize <=
+         GuardedCidBits::bitsize());
+}
 
 Representation Slot::UnboxedRepresentation() const {
   switch (field_guard_state().guarded_cid()) {
